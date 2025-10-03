f48e52dadbba91dd05577b63d4eeb071f1300f6e
Rudr7_7 <ankitgautam50366@gmail.com>
2025-10-03 15:37:15 +0530

    adding basic maths code

---
+++ Basic maths using C++/digit_extraction.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countDigit(int n) {
+        int count = 0 ;
+        while ( n > 0 ){
+            int lastDigit = n % 10;
+            count = count + 1;
+            n = n / 10;
+        }
+        return count;
+    }
+};
